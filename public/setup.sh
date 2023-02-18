28d65a7b6700fba0ff450474d7064876e9dea4b6
Mark R. Florkowski <mark.florkowski@gmail.com>
2023-02-17 22:34:19 -0800

    update setup.sh
    - dock position & disable recents
    - ohmyzsh last in script
    - remove dockutil after we're done with it
    - remove caffeine
    - rectangle pro settings config
    - three-finger drag
    - use default ssh-agent for commit signing 
    - add linear

--- public/setup.sh
+++ public/setup.sh
@@ -5,7 +5,7 @@
 brew update
 brew upgrade
 
-brew install --cask google-chrome visual-studio-code discord karabiner-elements shottr rectangle-pro 1password 1password-cli vlc hpedrorodrigues/tools/dockutil
+brew install --cask google-chrome visual-studio-code discord karabiner-elements shottr rectangle-pro 1password 1password-cli vlc hpedrorodrigues/tools/dockutil linear-linear
 brew install git fnm gh tmux pnpm
 
 # Set up dock icons
@@ -23,20 +23,8 @@ dockutil --add '/Applications' --view grid --display folder --no-restart
 dockutil --add '~/Documents' --view grid --display folder --no-restart
 dockutil --add '~/Downloads' --view grid --display folder
 
-# if on a desktop, install caffeine
-if [[ $(sysctl -n hw.model) = *MacStudio* ]]; then
-  echo "Desktop detected, installing caffeine"
-  brew install --cask caffeine
-fi
-
 # xcode command line tools
 xcode-select --install
-
-# oh-my-zsh
-sh -c "$(curl -# -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# zsh aliases
-echo "alias c='open \$1 -a \"Visual Studio Code\"'" >>~/.zshrc
 
 # oh-my-tmux
 cd ~
@@ -57,7 +45,7 @@ git config --global push.default upstream
 # commit signing with 1password
 git config --global user.signingkey "$(op item get "Github SSH Key" --fields "Public Key" --account RXTVLR6HMRES3CGS7G3WHSUPOQ)"
 git config --global gpg.format "ssh"
-git config --global gpg.ssh.program "/Applications/1Password.app/Contents/MacOS/op-ssh-sign"
+#git config --global gpg.ssh.program "/Applications/1Password.app/Contents/MacOS/op-ssh-sign"
 git config --global commit.gpgsign true
 
 # git aliases
@@ -79,6 +67,13 @@ curl -# https://gist.githubusercontent.com/markflorkowski/bc393361c0222f19ec3131
 
 curl -# https://gist.githubusercontent.com/markflorkowski/3774bbbfeccd539c4343058e0740367c/raw/7c6e711a9516f83ff48c99e43eef9ca13fb05246/1643178345.json -o ~/.config/karabiner/assets/complex_modifications/1643178345.json
 
+# configure rectangle pro to use icloud sync and launch on login
+echo "Updating RectanglePro config"
+/usr/libexec/PlistBuddy -c 'delete :iCloudSync' /Users/mrf/Library/Preferences/com.knollsoft.Hookshot.plist         
+/usr/libexec/PlistBuddy -c 'add :iCloudSync bool true' /Users/mrf/Library/Preferences/com.knollsoft.Hookshot.plist
+/usr/libexec/PlistBuddy -c 'delete :launchOnLogin' /Users/mrf/Library/Preferences/com.knollsoft.Hookshot.plist         
+/usr/libexec/PlistBuddy -c 'add :launchOnLogin bool true' /Users/mrf/Library/Preferences/com.knollsoft.Hookshot.plist
+
 echo "Updating macOS settings"
 
 # Disable annoying backswipe in Chrome
@@ -87,6 +82,16 @@ defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool false
 # Avoid the creation of .DS_Store files on network volumes or USB drives
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# Enable three-finger drag
+defaults write com.apple.AppleMultitouchTrackpad DragLock -bool false
+defaults write com.apple.AppleMultitouchTrackpad Dragging -bool false
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool true
+
+# Dock tweaks
+defaults write com.apple.dock orientation -string left # Move dock to left side of screen
+defaults write com.apple.dock show-recents -bool FALSE # Disable "Show recent applications in dock"
+killall Dock
 
 # Finder tweaks
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true            # Show all filename extensions
@@ -100,3 +105,13 @@ killall Finder
 echo "Starting services"
 open "/Applications/Rectangle Pro.app"
 open "/Applications/Karabiner-Elements.app"
+
+echo "Removing config programs"
+brew remove hpedrorodrigues/tools/dockutil
+
+# oh-my-zsh (must be last)
+sh -c "$(curl -# -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# zsh aliases
+echo "alias c='open \$1 -a \"Visual Studio Code\"'" >>~/.zshrc
+source ~/.zshrc
